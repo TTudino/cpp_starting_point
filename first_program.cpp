730a6443ddc6e359b3220f6f3733b972be8200ea
Tobia Tudino <36140468+TTudino@users.noreply.github.com>
2021-05-24 17:13:39 +0200

    Update first_program.cpp

--- first_program.cpp
+++ first_program.cpp
@@ -17,6 +17,6 @@ int main()
 	cout << "Enter your favorite number between 1 and 100: ";
 	cin >> favorite_number;
 	cout << "Amazing!! That's my favorite number too!" << endl;
-    cout << "No really!! " << favorite_number <<  " is my favorite number!" << endl;
+        cout << "No really!! " << favorite_number <<  " is my favorite number!" << endl;
 	return 0;
 }
